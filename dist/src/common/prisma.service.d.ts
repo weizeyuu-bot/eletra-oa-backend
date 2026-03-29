79df22a0a12b8cc0e9c3c0c51e5ea9fdf838e20c
eletra-oa <you@example.com>
2026-03-29 12:46:27 +0800

    chore: ignore build artifacts and untrack dist

--- dist/src/common/prisma.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-export declare class PrismaService extends PrismaClient {
-    private readonly logger;
-    constructor();
-    onModuleInit(): Promise<void>;
-    onModuleDestroy(): Promise<void>;
-}
