d12bf00bdb3f1ba1b4f06f43fef1442a16220c0b
eletra-oa <you@example.com>
2026-03-30 09:48:30 +0800

    chore: allow all CORS origins for dev tunnel

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://10.160.8.42:3000'],
+    origin: true,
     credentials: true,
   });
 
